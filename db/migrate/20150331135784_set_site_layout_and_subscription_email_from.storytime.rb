82c85618022afca8905bf65e7bc3963521abbdc8
Ben Roesch <bcroesch@gmail.com>
2015-04-09 17:10:51 -0500

    Update migration

--- db/migrate/20150331135784_set_site_layout_and_subscription_email_from.storytime.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from storytime (originally 20150302171722)
-class SetSiteLayoutAndSubscriptionEmailFrom < ActiveRecord::Migration
-  def up
-    Storytime::Migrators::V1.set_site_layout_and_subscription_email_from
-  end
-
-  def down
-  end
-end
