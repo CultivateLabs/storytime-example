95403c7efab9ec3eb4b6330ac62edd33e5428d5e
Ben Roesch <bcroesch@gmail.com>
2015-04-10 07:50:52 -0500

    Fix site layout migration

--- db/migrate/20150331135784_set_site_layout.storytime.rb
+++ db/migrate/20150331135784_set_site_layout.storytime.rb
@@ -1,7 +1,7 @@
 # This migration comes from storytime (originally 20150302171722)
 class SetSiteLayout < ActiveRecord::Migration
   def up
-    Storytime::Migrators::V1.set_site_layout_and_subscription_email_from
+    Storytime::Migrators::V1.set_site_layout
   end
 
   def down
